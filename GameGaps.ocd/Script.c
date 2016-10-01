51470c3a1c9014f8a0ce5b5defde7799ea71cc9b
Sven Eberhardt <sven2@goldwipf.de>
2016-09-30 23:38:30 -0400

    Fix cheating in Gaps

--- GameGaps.ocd/Script.c
+++ GameGaps.ocd/Script.c
@@ -25,15 +25,17 @@ func InitGame(array players)
 	{
 		var y0 = 45 + i * 40;
 		var y1 = y0 + 15;
-		var xm = Random(wdt - 20) + 10;
+		var xm = Random(wdt - 20) + 10, x;
 		if (!i && Inside(x, wdt/2-15, wdt/2+15)) { --i; continue; } // not directly below start
-		for (var x=xm-10; x<xm+9; ++x)
-		{
-			var ay0=y0, ay1=y1;
-			while (!GBackSolid(x,ay0)) ++ay0;
-			while (!GBackSolid(x,ay1)) --ay1;
-			DrawMaterialQuad("Tunnelbrick-brick", x,ay0, x+1,ay0, x+1,ay1+1, x,ay1+1, true);
-		}
+		var x0 = xm-10, x1 = xm+9;
+		var ay0 = y0, ay1 = y1;
+		var by0 = y0, by1 = y1;
+		while (!GBackSolid(x0,ay0)) ++ay0;
+		while (!GBackSolid(x0,ay1)) --ay1;
+		while (!GBackSolid(x0,by0)) ++by0;
+		while (!GBackSolid(x0,by1)) --by1;
+		DrawMaterialQuad("Tunnel-brickback", x0,ay0-4, x1+1,by0-4, x1+1,by1+5, x0,ay1+5, true);
+		DrawMaterialQuad("Tunnelbrick-brick", x0,ay0, x1+1,by0, x1+1,by1+1, x0,ay1+1, true);
 	}
 	return inherited(players);
 }
